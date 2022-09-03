1635bc7d1d2edbbf30fe746411b9b6a3ef391824
88IO <8810aoba@gmail.com>
2022-09-03 16:43:35 +0900

    [Fix] abc260aをsnake caseに修正

--- rust/abc260/src/bin/a.rs
+++ rust/abc260/src/bin/a.rs
@@ -3,13 +3,13 @@ use proconio::input;
 
 fn main() {
     input! {
-        S: String
+        s: String
     };
 
     let mut map = HashMap::<char, u32>::new();
 
-    for s in S.chars() {
-        *map.entry(s).or_insert(0) += 1;
+    for si in s.chars() {
+        *map.entry(si).or_insert(0) += 1;
     }
 
     for (k, v) in map {
